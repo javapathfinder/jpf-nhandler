06d534d4d29f4771ef8ea467ec2fc725fa6f2ea1
nastaran shafiei <nastaran.shafiei@gmail.com>
2012-05-23 14:40:13 -0700

    Extended the classes used to test the conversion from JPF to JVM.

--- src/tests/converter/JPF2JVMTest.java
+++ src/tests/converter/JPF2JVMTest.java
@@ -3,8 +3,21 @@ package converter;
 import gov.nasa.jpf.jvm.MJIEnv;
 import gov.nasa.jpf.util.test.TestJPF;
 
+import java.util.HashMap;
+
 import org.junit.Test;
 
+/**
+ * This class tests the conversion form JPF to JVM, and it works along its
+ * corresponding native peer which is JPF_converter_JPF2JVMTest.
+ * 
+ * The methods of this class create JPF objects and classes, and by invoking
+ * native methods, the JPF object/class is converted to the JVM ones in the
+ * native peer which if the object/class is created as expected.
+ * 
+ * @author Nastaran Shafiei
+ * @author Franck van Breugel
+ */
 public class JPF2JVMTest extends TestJPF {
   private final static String[] JPF_ARGS = {};
 
@@ -38,4 +51,44 @@ public class JPF2JVMTest extends TestJPF {
     }
   }
 
+  private native void convertArrayTest (String[] arr);
+
+  @Test
+  public void convertArrayTest (){
+    if (verifyNoPropertyViolation()){
+      String[] arr = { "e1", "e2", "e3" };
+      convertArrayTest(arr);
+    }
+  }
+
+  private native void convertHashMapTest (HashMap<Integer, String> map);
+
+  @Test
+  public void convertHashMapTest (){
+    if (verifyNoPropertyViolation()){
+      HashMap<Integer, String> map = new HashMap<Integer, String>();
+      map.put(0, "zero");
+      map.put(1, "one");
+      map.put(2, "two");
+      convertHashMapTest(map);
+    }
+  }
+
+  public static class TestConversion {
+    protected static int i = 0;
+
+    protected static void inc (int amount){
+      i += amount;
+    }
+  }
+
+  private native void convertClassTest (Class<?> cls);
+
+  @Test
+  public void convertClassTest (){
+    if (verifyNoPropertyViolation()){
+      TestConversion.inc(10);
+      convertClassTest(TestConversion.class);
+    }
+  }
 }
--- src/tests/converter/JPF_converter_JPF2JVMTest.java
+++ src/tests/converter/JPF_converter_JPF2JVMTest.java
@@ -5,23 +5,82 @@ import gov.nasa.jpf.nhandler.conversion.ConversionException;
 import gov.nasa.jpf.nhandler.conversion.Converter;
 import gov.nasa.jpf.util.test.TestJPF;
 
+import java.util.HashMap;
+
+import converter.JPF2JVMTest.TestConversion;
+
+/**
+ * This is a native peer class which is used to test the conversion from JPF to
+ * JVM. It works along with the test class JPF2JVMTest.
+ * 
+ * @author Nastaran Shafiei
+ * @author Franck van Breugel
+ */
 public class JPF_converter_JPF2JVMTest extends TestJPF {
 
   public static void convertStringTest__Ljava_lang_String_2__V (MJIEnv env, int objRef, int jpfRef) throws ConversionException{
-    String s1 = new String("Hello World");
     Converter converter = new Converter(env);
 
     // converting JPF String to JVM string
-    String s2 = (String) converter.getJVMObj(jpfRef);
+    String s1 = (String) converter.getJVMObj(jpfRef);
+
+    String s2 = new String("Hello World");
+
     assertEquals(s1, s2);
   }
 
   public static void convertIntegerTest__Ljava_lang_Integer_2__V (MJIEnv env, int objRef, int jpfRef) throws ConversionException{
-    Integer i1 = new Integer(100);
     Converter converter = new Converter(env);
 
     // converting JPF Integer to JVM Integer
-    Integer i2 = (Integer) converter.getJVMObj(jpfRef);
+    Integer i1 = (Integer) converter.getJVMObj(jpfRef);
+
+    Integer i2 = new Integer(100);
+
     assertEquals(i1, i2);
+  }
+
+  public static void convertArrayTest___3Ljava_lang_String_2__V (MJIEnv env, int objRef, int jpfArr) throws ConversionException{
+    Converter converter = new Converter(env);
+
+    // converting JPF String array to JVM String array
+    String[] arr1 = (String[]) converter.getJVMObj(jpfArr);
+
+    String[] arr2 = { "e1", "e2", "e3" };
+
+    assertEquals(arr1.length, arr2.length);
+    assertEquals(arr1[0], arr2[0]);
+    assertEquals(arr1[1], arr2[1]);
+    assertEquals(arr1[2], arr2[2]);
+  }
+
+  public static void convertHashMapTest__Ljava_util_HashMap_2__V (MJIEnv env, int objRef, int jpfMap) throws ConversionException{
+    Converter converter = new Converter(env);
+
+    // converting JPF HashMap to JVM HashMap
+    HashMap<Integer, String> map1 = (HashMap<Integer, String>) converter.getJVMObj(jpfMap);
+
+    HashMap<Integer, String> map2 = new HashMap<Integer, String>();
+    map2.put(0, "zero");
+    map2.put(1, "one");
+    map2.put(2, "two");
+
+    assertEquals(map1, map2);
+    assertEquals(map1.get(0), map2.get(0));
+    assertEquals(map1.get(1), map2.get(1));
+    assertEquals(map1.get(2), map2.get(2));
+  }
+
+  public static void convertClassTest__Ljava_lang_Class_2__V (MJIEnv env, int objRef, int jpfCls) throws ConversionException, IllegalArgumentException, SecurityException, IllegalAccessException, NoSuchFieldException{
+    Converter converter = new Converter(env);
+
+    // converting JPF Class to JVM Class
+    Class cls1 = (Class) converter.getJVMCls(jpfCls);
+
+    Class cls2 = TestConversion.class;
+
+    assertEquals(cls1, cls2);
+    assertEquals(TestConversion.i, 10);
+    assertEquals(cls1.getDeclaredFields()[0].getInt(cls1), 10);
   }
 }
