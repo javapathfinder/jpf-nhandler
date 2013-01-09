cadb6e5cb76b0bf0dd6a34e7edc8e3c38728cb18
nastaran shafiei <nastaran.shafiei@gmail.com>
2013-01-09 13:16:23 -0800

    Added two new examples to nhandler

---
+++ src/examples/GCalendar.java
@@ -0,0 +1,9 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class GCalendar {
+  public static void main (String[] args) {
+    Calendar calendar = new GregorianCalendar();
+    System.out.println(calendar);
+  }
+}
