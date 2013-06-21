cbd87263a892035779dea7724674efd2ee0f3439
Chinmay Dabral <chinmay.dabral@gmail.com>
2013-06-22 02:13:10 +0530

    Added JVM2JPF converter for java.lang.reflect.Method

---
+++ src/main/nhandler/conversion/jvm2jpf/JVM2JPFjava_lang_reflect_MethodConverter.java
@@ -0,0 +1,144 @@
+package nhandler.conversion.jvm2jpf;
+
+import gov.nasa.jpf.util.MethodInfoRegistry;
+import gov.nasa.jpf.vm.ClassInfo;
+import gov.nasa.jpf.vm.DynamicElementInfo;
+import gov.nasa.jpf.vm.JPF_java_lang_reflect_Method;
+import gov.nasa.jpf.vm.MJIEnv;
+import gov.nasa.jpf.vm.MethodInfo;
+import gov.nasa.jpf.vm.StaticElementInfo;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Iterator;
+
+import nhandler.conversion.ConversionException;
+import nhandler.conversion.ConverterBase;
+
+/**
+ * A JVM2JPFConverter to convert JVM java.lang.reflect.Method objects to their
+ * JPF counterparts
+ * 
+ * @author Chinmay Dabral
+ */
+
+public class JVM2JPFjava_lang_reflect_MethodConverter extends JVM2JPFConverter {
+
+  /**
+   * No static fields in both java.lang.reflect.Method model and JVM classes
+   */
+  @Override
+  protected void setStaticFields (Class<?> JVMCls, StaticElementInfo sei, MJIEnv env) throws ConversionException {
+    return;
+  }
+
+  /**
+   * This is never called in this case, everything is handled by the overridden
+   * getJPFObj() itself
+   */
+
+  @Override
+  protected void setInstanceFields (Object JVMObj, DynamicElementInfo dei, MJIEnv env) throws ConversionException {
+
+  }
+
+  /**
+   * We get the existing MethodInfo object associated with the method that the
+   * given JVM object points to. We then create a JPF Method object and set its
+   * regIdx field to point to the index of the corresponding MethodInfo in the
+   * MethodInfoRegistry
+   */
+  @Override
+  protected int getJPFObj (Object JVMObj, MJIEnv env) throws ConversionException {
+    int JPFRef = MJIEnv.NULL;
+    if (JVMObj != null) {
+      // First check if we already converted this object:
+      JPFRef = getExistingJPFRef(JVMObj, false, env);
+
+      if (JPFRef == MJIEnv.NULL) {
+        Method jvmMeth = (Method) JVMObj;
+        Class<?> methClass = jvmMeth.getDeclaringClass();
+        String paramString = "(" + Utilities.getMethodParamString(jvmMeth) + ")";
+
+        ClassInfo methCi = obtainJPFCls(methClass, env);
+        MethodInfo mi = methCi.getMethod(jvmMeth.getName(), paramString, false);
+        System.out.println("methodInfo: " + mi);// TODO: remove
+
+        // register methodinfo to get regIdx
+        MethodInfoRegistry registry = getMethodInfoRegistry();
+        int rIdx = registry.registerMethodInfo(mi);
+        // create a Method object, and set regIdx for it
+        JPFRef = getNewJPFMethodRef(env);
+        env.setIntField(JPFRef, "regIdx", rIdx);
+        // put in map
+        ConverterBase.updatedJPFObj.put(JPFRef, jvmMeth);
+      }
+    }
+    return JPFRef;
+  }
+
+  /**
+   * Reflectively get MethodInfoRegistry from JPF_java_lang_reflect_Method
+   * 
+   * @return the MethodInfoRegistry contained in
+   *         JPF_java_lang_reflect_Method.registry
+   */
+  private MethodInfoRegistry getMethodInfoRegistry () {
+    MethodInfoRegistry registry = null;
+    try {
+      Field registryField = JPF_java_lang_reflect_Method.class.getDeclaredField("registry");
+      registryField.setAccessible(true);
+      try {
+        registry = (MethodInfoRegistry) registryField.get(null);
+      } catch (IllegalArgumentException e) {
+        e.printStackTrace();
+      } catch (IllegalAccessException e) {
+        e.printStackTrace();
+      }
+    } catch (NoSuchFieldException e) {
+      e.printStackTrace();
+    } catch (SecurityException e) {
+      e.printStackTrace();
+    }
+    return registry;
+  }
+
+  @Override
+  protected int getExistingJPFRef (Object JVMObj, boolean update, MJIEnv env) throws ConversionException {
+    int JPFRef = MJIEnv.NULL;
+    boolean found = false;
+    if (ConverterBase.updatedJPFObj.containsValue(JVMObj)) {
+      Iterator<Integer> iterator = (ConverterBase.updatedJPFObj.keySet()).iterator();
+      Integer key;
+      while (!found && iterator.hasNext()) {
+        key = iterator.next();
+        Object value = ConverterBase.updatedJPFObj.get(key);
+        if (value == JVMObj) {
+          found = true;
+          JPFRef = key;
+        }
+      }
+    }
+
+    return JPFRef;
+  }
+
+  /**
+   * Create a new JPF object of java.lang.reflect.Method type
+   * 
+   * @param env
+   * @return The ref for the created object
+   */
+  private int getNewJPFMethodRef (MJIEnv env) {
+    int JPFRef = MJIEnv.NULL;
+    ClassInfo ci = null;
+    try {
+      ci = getJPFCls(Method.class, env);
+    } catch (ConversionException e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+    JPFRef = env.newObject(ci);
+    return JPFRef;
+  }
+}
--- src/main/nhandler/conversion/jvm2jpf/Utilities.java
+++ src/main/nhandler/conversion/jvm2jpf/Utilities.java
@@ -5,13 +5,14 @@ import gov.nasa.jpf.vm.Fields;
 import gov.nasa.jpf.vm.MJIEnv;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 import nhandler.conversion.ConversionException;
 
-/** 
+/**
  * Helper methods needed for the conversion from JVM to JPF
  * 
- * @author Nastaran Shafiei 
+ * @author Nastaran Shafiei
  */
 public class Utilities {
 
@@ -25,43 +26,43 @@ public class Utilities {
    * @return a new JPF array of the same type and same size as the given JVM
    *         array.
    */
-  public static int createNewJPFArray (Object JVMArr, MJIEnv env){
+  public static int createNewJPFArray (Object JVMArr, MJIEnv env) {
     String type = JVMArr.getClass().getName();
     int JPFArr = MJIEnv.NULL;
     // byte[]
-    if (type.equals("[B")){
+    if (type.equals("[B")) {
       JPFArr = env.newByteArray(((byte[]) JVMArr).length);
     }
     // char[]
-    else if (type.equals("[C")){
+    else if (type.equals("[C")) {
       JPFArr = env.newCharArray(((char[]) JVMArr).length);
     }
     // short[]
-    else if (type.equals("[S")){
+    else if (type.equals("[S")) {
       JPFArr = env.newShortArray(((short[]) JVMArr).length);
     }
     // int[]
-    else if (type.equals("[I")){
+    else if (type.equals("[I")) {
       JPFArr = env.newIntArray(((int[]) JVMArr).length);
     }
     // float[]
-    else if (type.equals("[F")){
+    else if (type.equals("[F")) {
       JPFArr = env.newFloatArray(((float[]) JVMArr).length);
     }
     // long[]
-    else if (type.equals("[J")){
+    else if (type.equals("[J")) {
       JPFArr = env.newLongArray(((long[]) JVMArr).length);
     }
     // double[]
-    else if (type.equals("[D")){
+    else if (type.equals("[D")) {
       JPFArr = env.newDoubleArray(((double[]) JVMArr).length);
     }
     // boolean[]
-    else if (type.equals("[Z")){
+    else if (type.equals("[Z")) {
       JPFArr = env.newBooleanArray(((boolean[]) JVMArr).length);
     }
     // Object[]
-    else{
+    else {
       JPFArr = env.newObjectArray(((Object[]) JVMArr).getClass().getComponentType().getName(), ((Object[]) JVMArr).length);
     }
 
@@ -69,8 +70,8 @@ public class Utilities {
   }
 
   /**
-   * Sets an element of the given JPF primitive array to the same value as the given JVM
-   * object.
+   * Sets an element of the given JPF primitive array to the same value as the
+   * given JVM object.
    * 
    * @param ei
    *          an object that represents the JPF array
@@ -87,25 +88,25 @@ public class Utilities {
    * @throws ConversionException
    *           if the given field is not of primitive type
    */
-  public static void setJPFPrimitiveField (ElementInfo ei, int index, Field fld, Object JVMObj) throws IllegalAccessException, ConversionException{
+  public static void setJPFPrimitiveField (ElementInfo ei, int index, Field fld, Object JVMObj) throws IllegalAccessException, ConversionException {
     Fields fields = ei.getFields();
-    if (fld.getType().getName().equals("boolean")){
+    if (fld.getType().getName().equals("boolean")) {
       fields.setBooleanValue(index, fld.getBoolean(JVMObj));
-    } else if (fld.getType().getName().equals("byte")){
+    } else if (fld.getType().getName().equals("byte")) {
       fields.setByteValue(index, fld.getByte(JVMObj));
-    } else if (fld.getType().getName().equals("int")){
+    } else if (fld.getType().getName().equals("int")) {
       fields.setIntValue(index, fld.getInt(JVMObj));
-    } else if (fld.getType().getName().equals("short")){
+    } else if (fld.getType().getName().equals("short")) {
       fields.setShortValue(index, fld.getShort(JVMObj));
-    } else if (fld.getType().getName().equals("long")){
+    } else if (fld.getType().getName().equals("long")) {
       fields.setLongValue(index, fld.getLong(JVMObj));
-    } else if (fld.getType().getName().equals("char")){
+    } else if (fld.getType().getName().equals("char")) {
       fields.setCharValue(index, fld.getChar(JVMObj));
-    } else if (fld.getType().getName().equals("float")){
+    } else if (fld.getType().getName().equals("float")) {
       fields.setFloatValue(index, fld.getFloat(JVMObj));
-    } else if (fld.getType().getName().equals("double")){
+    } else if (fld.getType().getName().equals("double")) {
       fields.setDoubleValue(index, fld.getDouble(JVMObj));
-    } else{
+    } else {
       throw new ConversionException("Unknown premitive type " + fld.getType().getName());
     }
   }
@@ -122,65 +123,117 @@ public class Utilities {
    * @throws ConversionException
    *           if the given array is not of primitive type
    */
-  public static void setJPFPrimitiveArr (ElementInfo ei, Object JVMObj, MJIEnv env) throws ConversionException{
+  public static void setJPFPrimitiveArr (ElementInfo ei, Object JVMObj, MJIEnv env) throws ConversionException {
     String type = ei.getType();
     // byte[]
-    if (type.equals("[B")){
+    if (type.equals("[B")) {
       byte[] JVMArr = (byte[]) JVMObj;
-      for (int i = 0; i < JVMArr.length; i++){
+      for (int i = 0; i < JVMArr.length; i++) {
         env.setByteArrayElement(ei.getObjectRef(), i, JVMArr[i]);
       }
     }
     // char[]
-    else if (type.equals("[C")){
+    else if (type.equals("[C")) {
       char[] JVMArr = (char[]) JVMObj;
-      for (int i = 0; i < JVMArr.length; i++){
+      for (int i = 0; i < JVMArr.length; i++) {
         env.setCharArrayElement(ei.getObjectRef(), i, JVMArr[i]);
       }
     }
     // short[]
-    else if (type.equals("[S")){
+    else if (type.equals("[S")) {
       short[] JVMArr = (short[]) JVMObj;
-      for (int i = 0; i < JVMArr.length; i++){
+      for (int i = 0; i < JVMArr.length; i++) {
         env.setShortArrayElement(ei.getObjectRef(), i, JVMArr[i]);
       }
     }
     // int[]
-    else if (type.equals("[I")){
+    else if (type.equals("[I")) {
       int[] JVMArr = (int[]) JVMObj;
-      for (int i = 0; i < JVMArr.length; i++){
+      for (int i = 0; i < JVMArr.length; i++) {
         env.setIntArrayElement(ei.getObjectRef(), i, JVMArr[i]);
       }
     }
     // float[]
-    else if (type.equals("[F")){
+    else if (type.equals("[F")) {
       float[] JVMArr = (float[]) JVMObj;
-      for (int i = 0; i < JVMArr.length; i++){
+      for (int i = 0; i < JVMArr.length; i++) {
         env.setFloatArrayElement(ei.getObjectRef(), i, JVMArr[i]);
       }
     }
     // long[]
-    else if (type.equals("[J")){
+    else if (type.equals("[J")) {
       long[] JVMArr = (long[]) JVMObj;
-      for (int i = 0; i < JVMArr.length; i++){
+      for (int i = 0; i < JVMArr.length; i++) {
         env.setLongArrayElement(ei.getObjectRef(), i, JVMArr[i]);
       }
     }
     // double[]
-    else if (type.equals("[D")){
+    else if (type.equals("[D")) {
       double[] JVMArr = (double[]) JVMObj;
-      for (int i = 0; i < JVMArr.length; i++){
+      for (int i = 0; i < JVMArr.length; i++) {
         env.setDoubleArrayElement(ei.getObjectRef(), i, JVMArr[i]);
       }
     }
     // boolean[]
-    else if (type.equals("[Z")){
+    else if (type.equals("[Z")) {
       boolean[] JVMArr = (boolean[]) JVMObj;
-      for (int i = 0; i < JVMArr.length; i++){
+      for (int i = 0; i < JVMArr.length; i++) {
         env.setBooleanArrayElement(ei.getObjectRef(), i, JVMArr[i]);
       }
-    } else{
+    } else {
       throw new ConversionException("Unknown array type " + type);
     }
   }
+
+  public static String getMethodParamString (Method meth) {
+    Class<?>[] params = meth.getParameterTypes();
+    StringBuilder r = new StringBuilder();
+    for (Class<?> c : params) {
+      String name = c.getName();
+      if (name.contains(".")) {
+        // non-primitive type, or array of non-primitive type
+        name = name.replace(".", "/");
+        if (!name.startsWith("[")) {
+          // non-primitive type (not array)
+          name = "L" + name + ";";
+        }
+      } else {
+        //primitive type, or array of primitive type
+        if(!name.startsWith("["))
+        {
+          try {
+            name = getPrimitiveTypeSymbol(name);
+          } catch (ConversionException e) {
+            e.printStackTrace();
+            System.exit(1);
+          }
+        }
+      }
+      r.append(name);
+    }
+    return r.toString();
+  }
+  
+  public static String getPrimitiveTypeSymbol(String type) throws ConversionException
+  {
+    if(type.equals("byte"))
+      return "B";
+    else if(type.equals("int"))
+      return "I";
+    else if(type.equals("boolean"))
+      return "Z";
+    else if(type.equals("char"))
+      return "C";
+    else if(type.equals("double"))
+      return "D";
+    else if(type.equals("float"))
+      return "F";
+    else if(type.equals("long"))
+      return "J";
+    else if(type.equals("short"))
+      return "S";
+    else
+      throw new ConversionException("Unknown primitive type");
+  }
+
 }
