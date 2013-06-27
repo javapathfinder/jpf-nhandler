b4667e1c52ed4b785d08dec14aae7d431b47ce14
Chinmay Dabral <chinmay.dabral@gmail.com>
2013-06-27 15:06:11 +0530

    Cleaned up JVM2JPF Field

--- src/main/nhandler/conversion/jvm2jpf/JVM2JPFjava_lang_reflect_FieldConverter.java
+++ src/main/nhandler/conversion/jvm2jpf/JVM2JPFjava_lang_reflect_FieldConverter.java
@@ -34,73 +34,36 @@ public class JVM2JPFjava_lang_reflect_FieldConverter extends JVM2JPFConverter {
   }
 
   /**
-   * Don't need to do anything, everything is handled by getJPFObj()
+   * Register the FieldInfo to get regIdx and set it on the Field object
    */
   @Override
   protected void setInstanceFields (Object JVMObj, DynamicElementInfo dei, MJIEnv env) throws ConversionException {
+    Field jvmField = (Field) JVMObj;
+    Class<?> fieldDeclClass = jvmField.getDeclaringClass();
+    boolean isStatic = ((Modifier.toString(jvmField.getModifiers())).indexOf("static") != -1);
 
-  }
+    ClassInfo fieldDeclCi = obtainJPFCls(fieldDeclClass, env);
+    FieldInfo fi = null;
+    if (isStatic)
+      fi = fieldDeclCi.getDeclaredStaticField(jvmField.getName());
+    else
+      fi = fieldDeclCi.getDeclaredInstanceField(jvmField.getName());
+    System.out.println("fieldInfo: " + fi);// TODO: remove
 
-  /**
-   * We get the required FieldInfo object from the ClassInfo, corresponding to
-   * the required field and register it. We then create a new JPF Field object
-   * and set its regIdx field got by registration
-   */
-  @Override
-  protected int getJPFObj (Object JVMObj, MJIEnv env) throws ConversionException {
-    int JPFRef = MJIEnv.NULL;
-    if (JVMObj != null) {
-      // First check if we already converted this object:
-      JPFRef = getExistingJPFRef(JVMObj, false, env);
-
-      if (JPFRef == MJIEnv.NULL) {
-        Field jvmField = (Field) JVMObj;
-        Class<?> fieldClass = jvmField.getDeclaringClass();
-        boolean isStatic = ((Modifier.toString(jvmField.getModifiers())).indexOf("static") != -1);
-
-        ClassInfo fieldCi = obtainJPFCls(fieldClass, env);
-        FieldInfo fi = null;
-        if (isStatic)
-          fi = fieldCi.getDeclaredStaticField(jvmField.getName());
-        else
-          fi = fieldCi.getDeclaredInstanceField(jvmField.getName());
-        System.out.println("fieldInfo: " + fi);// TODO: remove
-
-        // register FieldInfo to get regIdx
-        int rIdx = registerFieldInfo(env, fi);
-        // create a Method object, and set regIdx for it
-        JPFRef = getNewJPFFieldRef(env);
-        env.setIntField(JPFRef, "regIdx", rIdx);
-        // put in map
-        ConverterBase.updatedJPFObj.put(JPFRef, jvmField);
-      }
-    }
-    return JPFRef;
-  }
-
-  /**
-   * Create a new JPF java.lang.reflect.Field object
-   * @param env
-   * @return the ref for the created object
-   */
-  private int getNewJPFFieldRef (MJIEnv env) {
-    int JPFRef = MJIEnv.NULL;
-    ClassInfo ci = null;
-    try {
-      ci = getJPFCls(Field.class, env);
-    } catch (ConversionException e) {
-      e.printStackTrace();
-      System.exit(1);
-    }
-    JPFRef = env.newObject(ci);
-    return JPFRef;
+    // register FieldInfo to get regIdx and set it on the Field object
+    int rIdx = registerFieldInfo(env, fi);
+    ClassInfo fieldCi = obtainJPFCls(Field.class, env);
+    FieldInfo regFi = fieldCi.getInstanceField("regIdx");
+    dei.setIntField(regFi, rIdx);
   }
 
   /**
    * Register the given FieldInfo, by reflectively calling the registerFieldInfo
    * method of the JPF_java_lang_reflect_Field class
+   * 
    * @param env
-   * @param fi The FieldInfo to register
+   * @param fi
+   *          The FieldInfo to register
    * @return The regIdx from registration
    */
   private int registerFieldInfo (MJIEnv env, FieldInfo fi) {
@@ -124,25 +87,4 @@ public class JVM2JPFjava_lang_reflect_FieldConverter extends JVM2JPFConverter {
     }
     return regIdx;
   }
-
-  @Override
-  protected int getExistingJPFRef (Object JVMObj, boolean update, MJIEnv env) throws ConversionException {
-    int JPFRef = MJIEnv.NULL;
-    boolean found = false;
-    if (ConverterBase.updatedJPFObj.containsValue(JVMObj)) {
-      Iterator<Integer> iterator = (ConverterBase.updatedJPFObj.keySet()).iterator();
-      Integer key;
-      while (!found && iterator.hasNext()) {
-        key = iterator.next();
-        Object value = ConverterBase.updatedJPFObj.get(key);
-        if (value == JVMObj) {
-          found = true;
-          JPFRef = key;
-        }
-      }
-    }
-
-    return JPFRef;
-  }
-
 }
