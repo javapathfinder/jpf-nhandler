8cbd432dd74edfe42f43afe244f5b482ea68261a
Chinmay Dabral <chinmay.dabral@gmail.com>
2013-07-08 03:22:08 +0530

    JVM2JPF tests for java.text.DecimalFormat

--- src/tests/converter/specific/JPF_converter_specific_java_textTest.java
+++ src/tests/converter/specific/JPF_converter_specific_java_textTest.java
@@ -5,15 +5,20 @@ import java.text.DecimalFormat;
 import nhandler.conversion.ConversionException;
 import nhandler.conversion.ConverterBase;
 import nhandler.conversion.jpf2jvm.JPF2JVMConverter;
+import nhandler.conversion.jvm2jpf.JVM2JPFConverter;
 import gov.nasa.jpf.annotation.MJI;
 import gov.nasa.jpf.util.test.TestJPF;
 import gov.nasa.jpf.vm.MJIEnv;
 import gov.nasa.jpf.vm.NativePeer;
 
-public class JPF_converter_specific_JPF2JVMjava_textTest extends NativePeer{
+public class JPF_converter_specific_java_textTest extends NativePeer{
+
+  public static final String PATTERN1 = "0,0000.000", PATTERN2 = "#,#.000";
+  public static final String RESULT1 = "0,4242.420", RESULT2 = "4,2,4,2.420";
+  public static final double INPUT = 4242.42;
 
   @MJI
-  public static void convertDecimalFormatTest__Ljava_text_DecimalFormat_2__V
+  public static int convertDecimalFormatTest__Ljava_text_DecimalFormat_2___3Ljava_text_DecimalFormat_2
   (MJIEnv env, int objRef, int rFormat) {
     ConverterBase.reset(env);
     
@@ -24,8 +29,26 @@ public class JPF_converter_specific_JPF2JVMjava_textTest extends NativePeer{
       e.printStackTrace();
     }
     
-    DecimalFormat jvmFormat = new DecimalFormat(JPF2JVMjava_textTest.PATTERN);
+    DecimalFormat jvmFormat = new DecimalFormat(java_textTest.PATTERN);
     TestJPF.assertEquals(jvmFormat, format);
     TestJPF.assertEquals(jvmFormat.toPattern(), format.toPattern());
+    
+    /*
+     * We have to test two things:
+     * - returning the same object that was passed to us, after modifying it
+     * - returning a newly created object
+     */
+    format.applyPattern(PATTERN1);
+    DecimalFormat[] ret = new DecimalFormat[2];
+    ret[0] = format;
+    ret[1] = new DecimalFormat(PATTERN2);
+    
+    int JPFret = MJIEnv.NULL;
+    try {
+      JPFret = JVM2JPFConverter.obtainJPFObj(ret, env);
+    } catch (ConversionException e) {
+      e.printStackTrace();
+    }
+    return JPFret;
   }
 }
--- src/tests/converter/specific/java_textTest.java
+++ src/tests/converter/specific/java_textTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import gov.nasa.jpf.util.test.TestJPF;
 import gov.nasa.jpf.vm.MJIEnv;
 
-public class JPF2JVMjava_textTest extends TestJPF {
+public class java_textTest extends TestJPF {
   private final static String[] JPF_ARGS = {};
 
   private static MJIEnv env;
@@ -17,18 +17,29 @@ public class JPF2JVMjava_textTest extends TestJPF {
   }
 
   public static void setEnv (MJIEnv env) {
-    JPF2JVMjava_textTest.env = env;
+    java_textTest.env = env;
   }
-  
+
   public static final String PATTERN = "#,#####.#### ; $#";
 
-  private native void convertDecimalFormatTest (DecimalFormat df);
+  private native DecimalFormat[] convertDecimalFormatTest (DecimalFormat df);
 
   @Test
   public void convertDecimalFormatTest () {
     if (verifyNoPropertyViolation(JPF_ARGS)) {
       DecimalFormat format = new DecimalFormat(PATTERN);
-      convertDecimalFormatTest(format);
+      DecimalFormat[] retFormats = convertDecimalFormatTest(format);
+
+      /*
+       * The object that was passed was also returned as retFormats[0],
+       * so they should be equal
+       */
+      assertTrue(retFormats[0] == format);
+
+      assertEquals(JPF_converter_specific_java_textTest.RESULT1,
+                   retFormats[0].format(JPF_converter_specific_java_textTest.INPUT));
+      assertEquals(JPF_converter_specific_java_textTest.RESULT2,
+                   retFormats[1].format(JPF_converter_specific_java_textTest.INPUT));
     }
   }
 }
