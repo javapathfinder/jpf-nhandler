86ccc92d88ddacad00ca553a12b50e439cd7d670
Chinmay Dabral <chinmay.dabral@gmail.com>
2013-07-18 05:57:05 +0530

    Added tests for SimpleDateFormat

--- src/tests/converter/specific/JPF_converter_specific_java_textTest.java
+++ src/tests/converter/specific/JPF_converter_specific_java_textTest.java
@@ -1,6 +1,8 @@
 package converter.specific;
 
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import nhandler.conversion.ConversionException;
 import nhandler.conversion.ConverterBase;
@@ -13,9 +15,9 @@ import gov.nasa.jpf.vm.NativePeer;
 
 public class JPF_converter_specific_java_textTest extends NativePeer{
 
-  public static final String PATTERN1 = "0,0000.000", PATTERN2 = "#,#.000";
-  public static final String RESULT1 = "0,4242.420", RESULT2 = "4,2,4,2.420";
-  public static final double INPUT = 4242.42;
+  public static final String DF_PATTERN1 = "0,0000.000", DF_PATTERN2 = "#,#.000";
+  public static final String DF_RESULT1 = "0,4242.420", DF_RESULT2 = "4,2,4,2.420";
+  public static final double DF_INPUT = 4242.42;
 
   @MJI
   public static int convertDecimalFormatTest__Ljava_text_DecimalFormat_2___3Ljava_text_DecimalFormat_2
@@ -29,7 +31,7 @@ public class JPF_converter_specific_java_textTest extends NativePeer{
       e.printStackTrace();
     }
     
-    DecimalFormat jvmFormat = new DecimalFormat(java_textTest.PATTERN);
+    DecimalFormat jvmFormat = new DecimalFormat(java_textTest.DF_PATTERN);
     TestJPF.assertEquals(jvmFormat, format);
     TestJPF.assertEquals(jvmFormat.toPattern(), format.toPattern());
     
@@ -38,10 +40,51 @@ public class JPF_converter_specific_java_textTest extends NativePeer{
      * - returning the same object that was passed to us, after modifying it
      * - returning a newly created object
      */
-    format.applyPattern(PATTERN1);
+    format.applyPattern(DF_PATTERN1);
     DecimalFormat[] ret = new DecimalFormat[2];
     ret[0] = format;
-    ret[1] = new DecimalFormat(PATTERN2);
+    ret[1] = new DecimalFormat(DF_PATTERN2);
+    
+    int JPFret = MJIEnv.NULL;
+    try {
+      JPFret = JVM2JPFConverter.obtainJPFObj(ret, env);
+    } catch (ConversionException e) {
+      e.printStackTrace();
+    }
+    return JPFret;
+  }
+  
+  public static final String SDF_PATTERN1 = "YYYY-'W'ww", SDF_PATTERN2 = "yyyy.MM.dd G 'at' HH:mm:ss";
+  public static final String SDF_RESULT1 = "1983-W25", SDF_RESULT2= "1983.06.12 AD at 10:37:04";
+  public static final long SDF_INPUT = 424242424242L;
+  
+  /*
+   * This works the same as the above tests for DecimalFormat
+   */
+  @MJI
+  public static int convertSimpleDateFormatTest__Ljava_text_SimpleDateFormat_2___3Ljava_text_SimpleDateFormat_2
+  (MJIEnv env, int objRef, int rFormat) {
+    ConverterBase.reset(env);
+    
+    SimpleDateFormat format = null;
+    try {
+      format = (SimpleDateFormat) JPF2JVMConverter.obtainJVMObj(rFormat, env);
+    } catch (ConversionException e) {
+      e.printStackTrace();
+    }
+    
+    SimpleDateFormat jvmFormat = new SimpleDateFormat(java_textTest.SDF_PATTERN);
+    
+    TestJPF.assertEquals(jvmFormat, format);
+    
+    format.applyPattern(SDF_PATTERN1);
+    SimpleDateFormat[] ret = new SimpleDateFormat[2];
+    ret[0] = format;
+    ret[1] = new SimpleDateFormat(SDF_PATTERN2);
+    
+    Calendar calendar = Calendar.getInstance();
+    calendar.setTimeInMillis(SDF_INPUT);
+    System.out.println(ret[1].format(calendar.getTime()));
     
     int JPFret = MJIEnv.NULL;
     try {
--- src/tests/converter/specific/java_textTest.java
+++ src/tests/converter/specific/java_textTest.java
@@ -1,6 +1,8 @@
 package converter.specific;
 
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import org.junit.Test;
 
@@ -20,14 +22,14 @@ public class java_textTest extends TestJPF {
     java_textTest.env = env;
   }
 
-  public static final String PATTERN = "#,#####.#### ; $#";
+  public static final String DF_PATTERN = "#,#####.#### ; $#";
 
   private native DecimalFormat[] convertDecimalFormatTest (DecimalFormat df);
 
   @Test
   public void convertDecimalFormatTest () {
     if (verifyNoPropertyViolation(JPF_ARGS)) {
-      DecimalFormat format = new DecimalFormat(PATTERN);
+      DecimalFormat format = new DecimalFormat(DF_PATTERN);
       DecimalFormat[] retFormats = convertDecimalFormatTest(format);
 
       /*
@@ -36,10 +38,35 @@ public class java_textTest extends TestJPF {
        */
       assertTrue(retFormats[0] == format);
 
-      assertEquals(JPF_converter_specific_java_textTest.RESULT1,
-                   retFormats[0].format(JPF_converter_specific_java_textTest.INPUT));
-      assertEquals(JPF_converter_specific_java_textTest.RESULT2,
-                   retFormats[1].format(JPF_converter_specific_java_textTest.INPUT));
+      assertEquals(JPF_converter_specific_java_textTest.DF_RESULT1,
+                   retFormats[0].format(JPF_converter_specific_java_textTest.DF_INPUT));
+      assertEquals(JPF_converter_specific_java_textTest.DF_RESULT2,
+                   retFormats[1].format(JPF_converter_specific_java_textTest.DF_INPUT));
+    }
+  }
+  
+  public static final String SDF_PATTERN = "yyMMddHHmmss";
+  
+  private native SimpleDateFormat[] convertSimpleDateFormatTest(SimpleDateFormat sdf);
+  
+  /*
+   * This check is the same as for DecimalFormat
+   */
+  @Test
+  public void convertSimpleDateFormatTest() {
+    if(verifyNoPropertyViolation(JPF_ARGS)) {
+      SimpleDateFormat format = new SimpleDateFormat(SDF_PATTERN);
+      SimpleDateFormat[] retFormats = convertSimpleDateFormatTest(format);
+      
+      assertTrue(retFormats[0] == format);
+      
+      Calendar calendar = Calendar.getInstance();
+      calendar.setTimeInMillis(JPF_converter_specific_java_textTest.SDF_INPUT);
+
+      assertEquals(JPF_converter_specific_java_textTest.SDF_RESULT1,
+                   retFormats[0].format(calendar.getTime()));
+      assertEquals(JPF_converter_specific_java_textTest.SDF_RESULT2,
+                   retFormats[1].format(calendar.getTime()));
     }
   }
 }
