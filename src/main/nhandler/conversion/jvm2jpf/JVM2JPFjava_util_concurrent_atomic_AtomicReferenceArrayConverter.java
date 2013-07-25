81fb41caf40634e3d4cc9936e037d5be83288fa0
Chinmay Dabral <chinmay.dabral@gmail.com>
2013-07-26 03:23:39 +0530

    Added JVM2JPF for java.util.concurrent.atomic.AtomicReferenceArray

---
+++ src/main/nhandler/conversion/jvm2jpf/JVM2JPFjava_util_concurrent_atomic_AtomicReferenceArrayConverter.java
@@ -0,0 +1,50 @@
+package nhandler.conversion.jvm2jpf;
+
+import gov.nasa.jpf.vm.DynamicElementInfo;
+import gov.nasa.jpf.vm.MJIEnv;
+import gov.nasa.jpf.vm.StaticElementInfo;
+
+import java.lang.reflect.Field;
+import java.util.concurrent.atomic.AtomicReferenceArray;
+
+import nhandler.conversion.ConversionException;
+
+public class JVM2JPFjava_util_concurrent_atomic_AtomicReferenceArrayConverter extends JVM2JPFConverter {
+
+  @Override
+  protected void setStaticFields (Class<?> JVMCls, StaticElementInfo sei, MJIEnv env) throws ConversionException {
+
+  }
+
+  @Override
+  protected void setInstanceFields (Object JVMObj, DynamicElementInfo dei, MJIEnv env) throws ConversionException {
+    assert JVMObj instanceof AtomicReferenceArray : "Not the right converter";
+    
+    int JPFArrayRef = obtainJPFObj(getJVMArray((AtomicReferenceArray<?>) JVMObj), env);
+    dei.setReferenceField("array", JPFArrayRef);
+  }
+  
+  private Object[] getJVMArray(AtomicReferenceArray<?> JVMObj) {
+    Field arrayField = null;
+    try {
+      arrayField = AtomicReferenceArray.class.getDeclaredField("array");
+    } catch (NoSuchFieldException e1) {
+      e1.printStackTrace();
+    } catch (SecurityException e1) {
+      e1.printStackTrace();
+    }
+    arrayField.setAccessible(true);
+    
+    Object[] array = null;
+    try {
+      array = (Object[]) arrayField.get(JVMObj);
+    } catch (IllegalArgumentException e) {
+      e.printStackTrace();
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    }
+    
+    return array;
+  }
+
+}
