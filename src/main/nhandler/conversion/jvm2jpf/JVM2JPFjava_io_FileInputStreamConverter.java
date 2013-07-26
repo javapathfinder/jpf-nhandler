7b1088a43cd024cbc22a6560a2a1966ee7952b06
Chinmay Dabral <chinmay.dabral@gmail.com>
2013-07-26 21:04:03 +0530

    Added (incomplete) JVM2JPF for java.io.FileInputStream

---
+++ src/main/nhandler/conversion/jvm2jpf/JVM2JPFjava_io_FileInputStreamConverter.java
@@ -0,0 +1,129 @@
+package nhandler.conversion.jvm2jpf;
+
+import gov.nasa.jpf.util.DynamicObjectArray;
+import gov.nasa.jpf.vm.DynamicElementInfo;
+import gov.nasa.jpf.vm.MJIEnv;
+import gov.nasa.jpf.vm.NativePeer;
+import gov.nasa.jpf.vm.StaticElementInfo;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.Field;
+
+import nhandler.conversion.ConversionException;
+
+public class JVM2JPFjava_io_FileInputStreamConverter extends JVM2JPFConverter {
+
+  @Override
+  protected void setStaticFields (Class<?> JVMCls, StaticElementInfo sei, MJIEnv env) throws ConversionException {
+    
+  }
+
+  @Override
+  protected void setInstanceFields (Object JVMObj, DynamicElementInfo dei, MJIEnv env) throws ConversionException {
+    assert JVMObj instanceof FileInputStream;
+    
+    int JPFFd = constructJPFFileDescriptor((FileInputStream) JVMObj, env);
+    dei.setReferenceField("fd", JPFFd);
+  }
+  
+  private int constructJPFFileDescriptor(FileInputStream JVMObj, MJIEnv env) {
+    int JPFFd = env.newObject("java.io.FileDescriptor");
+    
+    NativePeer peer = env.getClassInfo(JPFFd).getNativePeer();
+    DynamicObjectArray<Object> array = getDynamicObjectArrayFromPeer(peer);
+    
+    int index = addIfNotPresent(array, JVMObj);
+    incrementNativePeerCounter(peer);
+    
+    // TODO: JPF FileDescriptor stores the filename (String fileName),
+    // Which we can't retrieve from a FileInputStream
+    // The field is used to reopen FileInputStreams when JPF backtracks
+    env.setReferenceField(JPFFd, "fileName", MJIEnv.NULL);
+    
+    // set state to FD_OPENED
+    int FD_OPENED = env.getClassInfo(JPFFd).getStaticElementInfo().getIntField("FD_OPENED");
+    env.setIntField(JPFFd, "state", FD_OPENED);
+    // set mode to FD_READ
+    int FD_READ = env.getClassInfo(JPFFd).getStaticElementInfo().getIntField("FD_READ");
+    env.setIntField(JPFFd, "mode", FD_READ);
+    // set offset to FileInputStream.position()
+    try {
+      env.setLongField(JPFFd, "off", JVMObj.getChannel().position());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    
+    return JPFFd;
+  }
+  
+  private void incrementNativePeerCounter(NativePeer peer) {
+    Field countField = null;
+    try {
+      countField = peer.getClass().getDeclaredField("count");
+    } catch (NoSuchFieldException e) {
+      e.printStackTrace();
+    } catch (SecurityException e) {
+      e.printStackTrace();
+    }
+    countField.setAccessible(true);
+    
+    int oldCount = 0;
+    try {
+      oldCount = countField.getInt(peer);
+    } catch (IllegalArgumentException e) {
+      e.printStackTrace();
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    }
+    
+    try {
+      countField.setInt(peer, oldCount + 1);
+    } catch (IllegalArgumentException e) {
+      e.printStackTrace();
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    }
+  }
+  
+  private DynamicObjectArray<Object> getDynamicObjectArrayFromPeer(NativePeer peer) {
+    DynamicObjectArray<Object> array = null;
+    
+    Field arrayField = null;
+    try {
+      arrayField = peer.getClass().getDeclaredField("content");
+    } catch (NoSuchFieldException e) {
+      e.printStackTrace();
+    } catch (SecurityException e) {
+      e.printStackTrace();
+    }
+    arrayField.setAccessible(true);
+    
+    try {
+      array = (DynamicObjectArray<Object>) arrayField.get(peer);
+    } catch (IllegalArgumentException e) {
+      e.printStackTrace();
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    }
+    
+    return array;
+  }
+  
+  private int addIfNotPresent(DynamicObjectArray<Object> array, FileInputStream fis) {
+    
+    for (int i = 0; i <= array.getMaxIndex(); i++) {
+      if (array.get(i) == fis) {
+        System.out.println("addIfNotPresent: present");
+        return i;
+      }
+    }
+    
+    System.out.println("addIfNotPresent: not present");
+    
+    array.set(array.getMaxIndex() + 1, fis);
+    
+    return array.getMaxIndex();
+  }
+
+}
